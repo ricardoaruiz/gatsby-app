82670d58294f77ff8abed87cda44778d1ec24869
Ricardo Ruiz <ricardo.almendro.ruiz@gmail.com>
2020-04-12 02:21:02 -0300

    Buscando dados com useStaticQuery

--- src/components/Profile/index.js
+++ src/components/Profile/index.js
@@ -1,32 +1,31 @@
 import React from "react"
-import { StaticQuery, graphql } from "gatsby"
+import { useStaticQuery, graphql } from "gatsby"
 
-const Profile = () => (
-  <StaticQuery
-    query={graphql`
-      query siteMetaData {
-        site {
-          siteMetadata {
-            author
-            position
-            description
-            title
-          }
+const Profile = () => {
+  const {
+    site: {
+      siteMetadata: { title, position, description },
+    },
+  } = useStaticQuery(graphql`
+    query siteMetaData {
+      site {
+        siteMetadata {
+          author
+          position
+          description
+          title
         }
       }
-    `}
-    render={({
-      site: {
-        siteMetadata: { title, position, description },
-      },
-    }) => (
-      <div className="profile-wrapper">
-        <h1>{title}</h1>
-        <h2>{position}</h2>
-        <p>{description}</p>
-      </div>
-    )}
-  />
-)
+    }
+  `)
+
+  return (
+    <div className="profile-wrapper">
+      <h1>{title}</h1>
+      <h2>{position}</h2>
+      <p>{description}</p>
+    </div>
+  )
+}
 
 export default Profile
