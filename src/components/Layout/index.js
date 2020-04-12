ca0f57b1042b63dd33e40757c0d46d2931a65dc0
Ricardo Ruiz <ricardo.almendro.ruiz@gmail.com>
2020-04-12 14:43:58 -0300

    Separando os estilos da parte logica do componente Layout

--- src/components/Layout/index.js
+++ src/components/Layout/index.js
@@ -1,34 +1,23 @@
 import React from "react"
 import PropTypes from "prop-types"
-import styled from "styled-components"
 
+import * as S from "./styled"
 import GlobalStyle from "../../styles/global"
 import Profile from "../Profile"
 
 const Layout = ({ children }) => {
   return (
-    <LayoutWrapper>
+    <S.LayoutWrapper>
       <GlobalStyle />
 
       <aside>
         <Profile />
       </aside>
 
-      <LayoutMain>{children}</LayoutMain>
-    </LayoutWrapper>
+      <S.LayoutMain>{children}</S.LayoutMain>
+    </S.LayoutWrapper>
   )
 }
-
-const LayoutWrapper = styled.section`
-  display: flex;
-`
-
-const LayoutMain = styled.main`
-  min-height: 100vh;
-  width: 100%;
-  padding: 0 3.75rem a 20rem;
-  background: #16202c;
-`
 
 Layout.propTypes = {
   children: PropTypes.node.isRequired,
---
+++ src/components/Layout/styled.js
@@ -0,0 +1,12 @@
+import styled from "styled-components"
+
+export const LayoutWrapper = styled.section`
+  display: flex;
+`
+
+export const LayoutMain = styled.main`
+  min-height: 100vh;
+  width: 100%;
+  padding: 0 3.75rem a 20rem;
+  background: #16202c;
+`
