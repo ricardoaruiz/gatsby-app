a37e3875db6c5fa91d3a6dc1ba3e268720229628
Ricardo Ruiz <ricardo.almendro.ruiz@gmail.com>
2020-04-12 01:37:40 -0300

    Criando a pagina About o componente Profile e fazendo a navegacao entre paginas

---
+++ src/components/Profile/index.js
@@ -0,0 +1,13 @@
+import React from "react"
+
+const Profile = () => {
+  return (
+    <div className="profile-wrapper">
+      <h1>Matt Foo</h1>
+      <h2>Desenvolvedor Web</h2>
+      <p>Tempor cillum quis irure amet eiusmod.</p>
+    </div>
+  )
+}
+
+export default Profile
---
+++ src/pages/about.js
@@ -0,0 +1,27 @@
+import React from "react"
+import Layout from "../components/layout"
+import SEO from "../components/seo"
+import { Link } from "gatsby"
+
+const AboutPage = () => {
+  return (
+    <Layout>
+      <SEO title="About" />
+      <h1>About</h1>
+      <ul>
+        <li>
+          <Link to="/" activeStyle={{ color: "red" }}>
+            Home
+          </Link>
+        </li>
+        <li>
+          <Link to="/about" activeStyle={{ color: "red" }}>
+            About
+          </Link>
+        </li>
+      </ul>
+    </Layout>
+  )
+}
+
+export default AboutPage
