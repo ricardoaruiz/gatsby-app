d8771ca1882139c22096fc10e9150a68bea653b8
Ricardo Ruiz <ricardo.almendro.ruiz@gmail.com>
2020-04-12 02:14:09 -0300

    Buscando dados com StaticQuery GraphQL

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,7 @@
 module.exports = {
   siteMetadata: {
     title: `My Blog`,
+    position: "Web developer",
     description: `A blog about frontend develpment and other cool stuff.`,
     author: `@myblog`,
   },
--- src/components/Profile/index.js
+++ src/components/Profile/index.js
@@ -1,13 +1,32 @@
 import React from "react"
+import { StaticQuery, graphql } from "gatsby"
 
-const Profile = () => {
-  return (
-    <div className="profile-wrapper">
-      <h1>Matt Foo</h1>
-      <h2>Desenvolvedor Web</h2>
-      <p>Tempor cillum quis irure amet eiusmod.</p>
-    </div>
-  )
-}
+const Profile = () => (
+  <StaticQuery
+    query={graphql`
+      query siteMetaData {
+        site {
+          siteMetadata {
+            author
+            position
+            description
+            title
+          }
+        }
+      }
+    `}
+    render={({
+      site: {
+        siteMetadata: { title, position, description },
+      },
+    }) => (
+      <div className="profile-wrapper">
+        <h1>{title}</h1>
+        <h2>{position}</h2>
+        <p>{description}</p>
+      </div>
+    )}
+  />
+)
 
 export default Profile
