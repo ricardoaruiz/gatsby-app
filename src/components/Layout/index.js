6e8c62b3dcd9f594c7588d0881734d10c7c0813a
Ricardo Ruiz <ricardo.almendro.ruiz@gmail.com>
2020-04-12 14:39:45 -0300

    Criando estilo global e adicionando no componente Layout

--- src/components/Layout/index.js
+++ src/components/Layout/index.js
@@ -2,14 +2,18 @@ import React from "react"
 import PropTypes from "prop-types"
 import styled from "styled-components"
 
+import GlobalStyle from "../../styles/global"
 import Profile from "../Profile"
 
 const Layout = ({ children }) => {
   return (
     <LayoutWrapper>
+      <GlobalStyle />
+
       <aside>
         <Profile />
       </aside>
+
       <LayoutMain>{children}</LayoutMain>
     </LayoutWrapper>
   )
@@ -23,7 +27,7 @@ const LayoutMain = styled.main`
   min-height: 100vh;
   width: 100%;
   padding: 0 3.75rem a 20rem;
-  background: #162202;
+  background: #16202c;
 `
 
 Layout.propTypes = {
