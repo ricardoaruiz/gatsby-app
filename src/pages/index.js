2bca80640a27b98f3585ca78b1fdfb913298b35d
Ricardo Ruiz <ricardo.almendro.ruiz@gmail.com>
2020-04-15 12:58:04 -0300

    Pegando os dados dos Posts dos Markdowns e exibindo na tela

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,22 +1,57 @@
 import React from "react"
+import { useStaticQuery, graphql } from "gatsby"
 
 import Layout from "../components/Layout"
 import SEO from "../components/seo"
 import PostItem from "../components/PostItem"
 
-const IndexPage = () => (
-  <Layout>
-    <SEO title="Home" />
-    <PostItem
-      slug="/about"
-      background="blue"
-      category="Misc"
-      date="30 de Julho de 2019"
-      timeToRead="5 minutos de leitura"
-      title="Diga não ao Medium: tenha sua própria plataforma"
-      description="Algumas razões para você ter a sua própria plataforma ao invés de soluções como o Medium."
-    />
-  </Layout>
-)
+const IndexPage = () => {
+  // Consultando os posts
+  const { allMarkdownRemark } = useStaticQuery(graphql`
+    query PostList {
+      allMarkdownRemark {
+        edges {
+          node {
+            id
+            frontmatter {
+              background
+              category
+              date(locale: "pt-br", formatString: "DD [de] MMMM [de] YYYY")
+              description
+              title
+            }
+            timeToRead
+          }
+        }
+      }
+    }
+  `)
+
+  const postLists = allMarkdownRemark.edges
+
+  return (
+    <Layout>
+      <SEO title="Home" />
+      {postLists.map(
+        ({
+          node: {
+            frontmatter: { background, category, description, date, title },
+            timeToRead,
+          },
+        }) => (
+          <PostItem
+            slug="/about"
+            background={background}
+            category={category}
+            date={date}
+            timeToRead={`${timeToRead} minutos de leitura`}
+            title={title}
+            description={description}
+          />
+        )
+      )}
+    </Layout>
+  )
+}
 
 export default IndexPage
