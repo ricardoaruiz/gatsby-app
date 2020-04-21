18bb2ce1d33e9dde69bca79d738116f0a511c0f8
Ricardo Ruiz <ricardo.almendro.ruiz@gmail.com>
2020-04-21 17:24:11 -0300

    Habilitando o plugin offline para funcionamento PWA

--- gatsby-config.js
+++ gatsby-config.js
@@ -38,11 +38,11 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
+        name: `Jon Doe`,
+        short_name: `Jon Doe`,
         start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
+        background_color: `#16202c`,
+        theme_color: `#16202c`,
         display: `minimal-ui`,
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
@@ -74,6 +74,6 @@ module.exports = {
     `gatsby-plugin-sitemap`,
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
-    // `gatsby-plugin-offline`,
+    `gatsby-plugin-offline`,
   ],
 }
